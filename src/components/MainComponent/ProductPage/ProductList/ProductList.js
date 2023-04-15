f7e2693aeb6ffe4aa924decc5eb984a7ef098d2e
mtan <70209314+tuthoeng123@users.noreply.github.com>
2023-04-15 16:16:06 +0700

    pagination

--- src/components/MainComponent/ProductPage/ProductList/ProductList.js
+++ src/components/MainComponent/ProductPage/ProductList/ProductList.js
@@ -1,7 +1,10 @@
 import Product from "./Product/Product";
 import React, { useState, useEffect } from 'react';
+
 function ProductList({ sortProducts, filterApplied, minPrice, maxPrice }) {
     const [products, setProducts] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [productsPerPage, setProductsPerPage] = useState(6);
 
     useEffect(() => {
         async function fetchProducts() {
@@ -17,24 +20,43 @@ function ProductList({ sortProducts, filterApplied, minPrice, maxPrice }) {
         fetchProducts();
     }, []);
 
+    // Filter and sort products
     let filteredProducts = products;
     if (filterApplied) {
         filteredProducts = products.filter(
             (product) => product.price >= minPrice && product.price <= maxPrice
         );
     }
-
     const sortedProducts = sortProducts(filteredProducts);
 
+    // Pagination
+    const indexOfLastProduct = currentPage * productsPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+    const currentProducts = sortedProducts.slice(indexOfFirstProduct, indexOfLastProduct);
+    const totalPages = Math.ceil(sortedProducts.length / productsPerPage);
+
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
     return (
-        <div className="row">
-            {sortedProducts.map((product) => (
-                <Product key={product.id} {...product} />
-            ))}
-        </div>
+        <>
+            <div className="row">
+                {currentProducts.map((product) => (
+                    <Product key={product.id} {...product} />
+                ))}
+            </div>
+            <nav>
+                <ul className="pagination justify-content-center">
+                    {Array.from({ length: totalPages }, (_, i) => (
+                        <li key={i} className={`page-item ${i + 1 === currentPage ? 'active' : ''}`}>
+                            <button onClick={() => paginate(i + 1)} className="page-link">
+                                {i + 1}
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </>
     );
 }
 
 export default ProductList;
-
-
--- src/components/MainComponent/ProductPage/ProductPage.js
+++ src/components/MainComponent/ProductPage/ProductPage.js
@@ -213,21 +213,7 @@ function ProductPage() {
 
                             <hr />
 
-                            <footer class="d-flex mt-4">
 
-                                <nav class="ms-3">
-                                    <ul class="pagination">
-                                        <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                        <li class="page-item active" aria-current="page">
-                                            <span class="page-link">2</span>
-                                        </li>
-                                        <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                        <li class="page-item">
-                                            <a class="page-link" href="#">Next</a>
-                                        </li>
-                                    </ul>
-                                </nav>
-                            </footer>
 
                         </main>
                     </div>
