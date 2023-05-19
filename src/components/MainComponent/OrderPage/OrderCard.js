de2db9dad0f80cfe0d89bdc03ad30abe8b502a96
Tuan Anh em <talama191@gmail.com>
2023-05-19 11:39:03 +0700

    order history page  done

--- src/components/MainComponent/OrderPage/OrderCard.js
+++ src/components/MainComponent/OrderPage/OrderCard.js
@@ -1,16 +1,40 @@
 import { useEffect, useState } from "react";
+import { GetCartLinesByCartID } from "../../../services/CartServices";
+import OrderProductSummary from "./OrderProductSummary";
 
 function OrderCard(props) {
     const [ordered_date, setOrderedDate] = useState("");
-
+    const [completed_date, setCompletedDate] = useState("");
+    const [cartLines, setCartLines] = useState([]);
+    const [totalPrice, setTotalPrice] = useState(0);
     useEffect(() => {
-        var t = props.ordered_at.split(/[- T+.:]/);
-        var d = new Date(Date.UTC(t[0], t[1] - 1, t[2], t[3], t[4], t[5]));
-        console.log(d);
-        setOrderedDate(d);
+        var t_order = "";
+        if (props.ordered_at !== null) {
+            t_order = props.ordered_at.split(/[- T+.:]/);
+        }
+        var c_order = "";
+        if (props.completed_at !== null) {
+            c_order = props.completed_at.split(/[- T+.:]/);
+        }
+        var d_order = new Date(Date.UTC(t_order[0], t_order[1] - 1, t_order[2], t_order[3], t_order[4], t_order[5]));
+        var c_order = new Date(Date.UTC(c_order[0], c_order[1] - 1, c_order[2], c_order[3], c_order[4], c_order[5]));
+        setOrderedDate(d_order);
+        setCompletedDate(c_order)
+        GetCartLines();
+
     }, []);
+    async function GetCartLines() {
+        var result = await GetCartLinesByCartID(props.cart_id);
+        setCartLines(result.data);
+        var total = 0;
+        for (var i = 0; i < result.data.length; i++) {
+            var value = result.data[i].quantity * result.data[i].product.price;
+            total += value;
+        }
+        setTotalPrice(total);
+    }
     return (
-        <div class="card-body row ml-5 mr-5 mb-3 border rounded-4">
+        <div class="card-body row ml-5 mr-5 mb-3 border rounded-4 bg-light bg-gradient">
             <div >
                 <div >
                     <h3>Order id: {props.cart_id}</h3>
@@ -21,17 +45,28 @@ function OrderCard(props) {
                 </div>
                 {props.cart_status === 2 ? (
                     <div>
-                        <span >Completed at: {props.completed_at}</span>
+                        <span >Completed at: {completed_date.toString()}</span>
                     </div>
                 ) : (<div></div>)}
                 <div class="d-flex">
-                    <h5>Total product: 5</h5>
-                    <h5 class="ml-auto" >Total price: $5.00</h5>
+                    <h5>Total product: {cartLines.length}</h5>
+                    <h5 class="ml-auto" >Total price: ${totalPrice}</h5>
                 </div>
             </div>
-            <div class="d-flex justify-content-center">
-                <button tpye="button" class="btn btn-primary">Order Detail</button>
-            </div>
+            <article class="filter-group border rounded-6 bg-warning bg-gradient ">
+                <header class="card-header">
+                    <a href="#" class="title" data-bs-toggle="collapse" data-bs-target={`#collapse-order-card-${props.cart_id}`}>
+                        <i class="icon-control fa fa-chevron-down"></i>Order detail
+                    </a>
+                </header>
+                <div class="collapse hide" id={`collapse-order-card-${props.cart_id}`}>
+                    {cartLines.map((cartLine) => (
+                        <OrderProductSummary
+                            key={cartLine.product_id}{...cartLine} />
+                    ))}
+                </div>
+            </article>
+
         </div>
     )
 }
--- src/components/MainComponent/OrderPage/OrderList.js
+++ src/components/MainComponent/OrderPage/OrderList.js
@@ -16,7 +16,12 @@ function OrderList() {
     function GetIncompleteOrders() {
         return carts.filter((cart) => {
             return cart.cart_status == 1;
-        })
+        }).reverse();
+    }
+    function GetCompletedOrders(){
+        return carts.filter((cart) => {
+            return cart.cart_status == 2;
+        }).reverse();
     }
     return (
         <div>
@@ -24,23 +29,34 @@ function OrderList() {
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-12">
-                            <div class="card border shadow-0">
+                            <div class="card border shadow-0 bg-secondary">
                                 <h4 class="cart-title mt-3 ml-3 mb-4">
                                     Your Order History
                                 </h4>
-                                <article class="filter-group border rounded-6 ml-5 mr-5 mb-5">
+                                <article class="bg-info bg-gradient filter-group border rounded-6 ml-5 mr-5 mb-5">
                                     <header class="card-header">
                                         <a href="#" class="title" data-bs-toggle="collapse" data-bs-target="#collapse_aside1">
-                                            <i class="icon-control fa fa-chevron-down"></i>Incomplete Order
+                                            <i class="icon-control fa fa-chevron-down"></i>Incomplete Order: {GetIncompleteOrders().length}
                                         </a>
                                     </header>
-                                    <div class="collapse show" id="collapse_aside1">
+                                    <div class="collapse hide" id="collapse_aside1">
                                         {GetIncompleteOrders().map((cart) => (
                                             <OrderCard key={cart.cart_id}{...cart} />
                                         ))}
                                     </div>
                                 </article>
-
+                                <article class="bg-success bg-gradient  filter-group border rounded-6 ml-5 mr-5 mb-5">
+                                    <header class="card-header">
+                                        <a href="#" class="title" data-bs-toggle="collapse" data-bs-target="#collapse_aside2">
+                                            <i class="icon-control fa fa-chevron-down"></i>Completed Order: {GetCompletedOrders().length}
+                                        </a>
+                                    </header>
+                                    <div class="collapse hide" id="collapse_aside2">
+                                        {GetCompletedOrders().map((cart) => (
+                                            <OrderCard key={cart.cart_id}{...cart} />
+                                        ))}
+                                    </div>
+                                </article>  
                             </div>
                         </div>
                     </div>
